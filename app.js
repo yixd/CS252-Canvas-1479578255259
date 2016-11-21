0d3aa1d6d76ec907f68e57a94bb2ddd78b08347d
yixd <broocdin@gmail.com>
2016-11-21 00:42:01 -0500

    basic functionality for drawing && chat

--- app.js
+++ app.js
@@ -10,31 +10,35 @@ var express = require('express');
 var app = express();
 var cfenv = require('cfenv');
 var appEnv = cfenv.getAppEnv();
-var http = require('http').Server(app);
+/*
+var http = require('http').Server(app);*/
+var http = app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
 var io = require('socket.io')(http);
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
+var history = [];
 io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-http.listen(appEnv.port, function(){
-    console.log('listening on *:' + appEnv.url);
-})
-/*
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
-*/
-
-/*
-io.sockets.on('connection', function(socket) {
-    socket.on('mousemove', function(data) {
-        socket.broadcast.emit('moving', data);
+    console.log('a user connected'); 
+    socket.on('chat', function(msg){
+        console.log('message:' + msg);
+        io.emit('chat', msg);
     });
+    for (var i in history) {
+        socket.emit('moving', history[i]);
+    }
+    socket.on('mousemove', function(data){
+        history.push(data);
+        /*socket.broadcast.emit('moving', data); */
+        io.emit('moving', data);
+    });
+    /* 清空画布 - 还没做 
+    socket.on('clearcanvas', function(){
+        history = [];
+        io.emit('clearcanvas'); 
+    });*/
 });
-*/
--- public/script.js
+++ public/script.js
@@ -2,25 +2,66 @@
 /*global $, jQuery, alert*/
 
 $(function () {
-    /*var c=document.getElementById("canv");
-    var c = $('#canv');
-    var cxt=c.getContext("2d");
-    cxt.moveTo(10,20);
-    cxt.lineTo(200,50);
-    cxt.lineTo(12,100);
-    cxt.stroke();
-    var doc = $(document),
-        socket = io();*/
+    var socket = io();
+    $('form').submit(function () {
+        socket.emit('chat', $('#msg').val());
+        $('#msg').val('');
+        return false;
+    });
+    socket.on('chat', function (msg) {
+        $('#chatbox').append($('<li>').text(msg));
+        var div = $('#msgarea');
+        div.scrollTop(div.prop("scrollHeight"));
+    });
     
+    var pos = {
+        fx: 0, fy: 0, tx: 0, ty: 0, color: '#000'
+    };
+    
+    var drawing = false, 
+        mov = false,
+        canvas = document.getElementById('canv');
+        ctx = canvas.getContext('2d');
+    
+    socket.on('moving', function (data) {
+        ctx.beginPath();
+        ctx.moveTo(data.fx, data.fy);
+        ctx.lineTo(data.tx, data.ty);
+        ctx.stroke();
+    });
+    canvas.onmousedown = function (e) { console.log('mouse down'); drawing = true; };
+    canvas.onmouseup = function (e) { drawing = false; };
+    canvas.onmousemove = function (e) {
+        pos.tx = e.pageX - canvas.offsetLeft;
+        pos.ty = e.pageY - canvas.offsetTop;
+        mov = true;
+        document.getElementById("zobc").innerHTML="Coordinates: (" + pos.tx + "," + pos.ty + ")";
+        document.getElementById("canv").style.border="2px solid #F08080";
+    };
+    canvas.onmouseout = function (e) {
+        drawing = false;
+        document.getElementById("zobc").innerHTML="";
+        document.getElementById("canv").style.border="2px solid #c3c3c3";
+    }
+    function mainloop() {
+        if(drawing && mov) {
+            socket.emit('mousemove', pos);
+            mov = false;
+        }
+        pos.fx = pos.tx;
+        pos.fy = pos.ty;
+        setTimeout(mainloop, 10);
+    }
+    mainloop();
 });
 
 function highlightcanvas(e) {
     x=e.clientX;
     y=e.clientY;
-    document.getElementById("stat").innerHTML="Coordinates: (" + x + "," + y + ")";
+    document.getElementById("zobc").innerHTML="Coordinates: (" + x + "," + y + ")";
     document.getElementById("canv").style.border="2px solid #F08080";
 }
 function dehighlightcanvas() {
-    document.getElementById("stat").innerHTML="";
+    document.getElementById("zobc").innerHTML="";
     document.getElementById("canv").style.border="2px solid #c3c3c3";
 }
